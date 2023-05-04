3504cb9b250175649b47a1b74b08857bddcbe68f
Maja Frankowska <majafrankowska88@gmail.com>
2023-05-04 02:29:24 +0200

    Add files via upload

---
+++ HeartAnimation.py
@@ -0,0 +1,16 @@
+
+from turtle import *
+
+color("red")
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
+done()
+
+#it's possible to switch from red to other color like pink
